a8e42c392c4e4d3d1b5e90abf0c815657e816aca
Viktor Benei <viktor.benei@gmail.com>
2018-10-14 17:18:55 +0200

    Added bitrise.yml upload option, and better styling
    Less verbose output, and color highlight.

--- create-app.sh
+++ create-app.sh
@@ -1,36 +1,78 @@
 #!/usr/bin/env bash
-set -ex
+
+#
+# Usage: set the input environment variables and then run: $ ./create-app.sh
+# Required input env vars:
+# * BITRISE_PERSONAL_ACCESS_TOKEN : your Bitrise.io API Token / Personal Access Token
+# * REPO_URL : Repository URL for git clone.
+# Optional intput env vars:
+# * BITRISE_YML_PATH : path to the bitrise.yml file to set as the config of the app.
+#     If not set then a very basic, generic config will be used on bitrise.io
+#     which basically just clones the repository.
+#
+
+# fail if any command fails
+set -e
+# enable debug mode
+# set -x
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 function check_required_env_var {
     env_var_key="$1"
     eval env_var='$'"${env_var_key}"
     if [ -z "$env_var" ] ; then
-        echo "[!] Missing input (env var): ${env_var_key}"
+        echo -e "${RED}[!] Missing required input (env var)${NC}: ${env_var_key}"
         exit 1
     fi
-    echo "(i) env_var_key: ${env_var}"
 }
 
 check_required_env_var 'BITRISE_PERSONAL_ACCESS_TOKEN'
 check_required_env_var 'REPO_URL'
 
 # register repo
+echo
+echo -e "${YELLOW}# Register repository ...${NC}"
 app_id="$(curl -H "Authorization: token ${BITRISE_PERSONAL_ACCESS_TOKEN}" \
   'https://api.bitrise.io/v0.1/apps/register' \
   -d '{"provider":"custom","is_public":false,"repo_url":"'"${REPO_URL}"""'''","type":"git"}' | jq -r '.slug')"
 echo "Registered app ID: ${app_id}"
 
 # finish
+echo
+echo -e "${YELLOW}# Set stack and finish registration ...${NC}"
 finish_call_response="$(curl --fail \
   -H "Authorization: token ${BITRISE_PERSONAL_ACCESS_TOKEN}" \
   "https://api.bitrise.io/v0.1/apps/${app_id}/finish" \
   -d '{"stack_id":"osx-xcode-edge","mode":"manual"}')"
 
-is_webhook_auto_reg_supported="$(echo "$finish_call_response" | jq -r '.is_webhook_auto_reg_supported')"
+# Upload bitrise.yml / build config if specified
+echo
+echo -e "${YELLOW}# Upload bitrise.yml ...${NC}"
+if [ ! -z "${BITRISE_YML_PATH}" ] ; then
+    jq -n --arg yml_content "$(cat "$BITRISE_YML_PATH")" \
+        '{app_config_datastore_yaml: $yml_content }' | curl --fail \
+        -X POST \
+        -H "Authorization: token ${BITRISE_PERSONAL_ACCESS_TOKEN}" \
+        "https://api.bitrise.io/v0.1/apps/${app_id}/bitrise.yml" \
+        -d@-
+else
+    echo "No bitrise.yml path specified (BITRISE_YML_PATH env var),"
+    echo " so a very basic config will be used."
+    echo "You can of course change this any time on the Workflow tab of the app on bitrise.io"
+fi
+echo
 
+# Auto-register an incoming webhook on supported services (GitHub, Bitbucket, GitLab)
+# so that a code push will trigger a build automatically.
+# First check if the service is supported & that we have the required
+# connected account so that Bitrise can register the webhook:
+echo
+echo -e "${YELLOW}# Register incoming webhook ...${NC}"
+is_webhook_auto_reg_supported="$(echo "$finish_call_response" | jq -r '.is_webhook_auto_reg_supported')"
 if [[ "$is_webhook_auto_reg_supported" == "true" ]] ; then
-    # register a webhook on supported services (GitHub, Bitbucket, GitLab)
-    # so that a code push will trigger a build automatically
     curl --fail \
         -X POST \
         -H "Authorization: token ${BITRISE_PERSONAL_ACCESS_TOKEN}" \
@@ -40,9 +82,9 @@ else
     echo "You can find the related instructions on the Code tab of the app on bitrise.io,"
     echo " in the INCOMING WEBHOOKS section: https://app.bitrise.io/app/${app_id}#/code"
 fi
+echo
 
 # TODO:
-# - upload the bitrise.yml / build config
 # - register SSH key
 #     - step 1: generate ssh key
 #     - step 2: register it for the project
